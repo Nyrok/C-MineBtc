861b5572cbe16612a7015d6e61b59aff99b385c9
Hamza KONTE <47859726+Nyrok@users.noreply.github.com>
2025-03-31 17:36:33 +0200

    Add process group management

--- main.c
+++ main.c
@@ -1,3 +1,4 @@
+// ldd: -lcrypto
 #include <openssl/md5.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -8,6 +9,7 @@
 
 #define OUTPUT_FILE "found."
 #define DIFFICULTY 6
+#define READ_BUFFER 10 // Int Max
 
 // = {0} => all the array is reset to zero (only works for zero!)
 char hash[1 + 2*MD5_DIGEST_LENGTH] = {0};
@@ -43,13 +45,8 @@ int intlen (int n){
 void bruteforce (int first, int step, int zero){
     char *str;
     char *nonce;
+    pid_t pid = getpid();
 
-    pid_t ppid = getpid();
-    pid_t pid = fork();
-    if (setpgid(pid, ppid) == -1 || pid < 0){
-        perror(pid < 0 ? "fork" : "setpgid");
-        exit(EXIT_FAILURE);
-    }
     while (1) {
         str = malloc((intlen(first) + 1) * sizeof(char));
         if (!str){
@@ -58,34 +55,82 @@ void bruteforce (int first, int step, int zero){
         }
         sprintf(str, "%d", first);
         if (zeros(md5hash((unsigned char *)str), zero)){
+            nonce = strdup(str);
             free(str);
             break;
         }
         free(str);
         first += step;
     }
-    if (ppid == getpid()){
-        nonce = malloc((intlen(first) + 1) * sizeof(char));
-        sprintf(nonce, "%d", first);
-        str = malloc((strlen(OUTPUT_FILE) + intlen(pid) + 1) * sizeof(char));
-        sprintf(str, "%s%d", OUTPUT_FILE, pid);
-        int fd = open(str, O_WRONLY | O_CREAT, 470);
-        if (fd == -1){
-            perror("open");
-            exit(EXIT_FAILURE);
-        }
-        write(fd, nonce, strlen(nonce));
-        if (close(fd) == -1){
-            perror("close");
-            exit(EXIT_FAILURE);
-        }
-        kill(ppid, SIGKILL);
+    str = malloc((strlen(OUTPUT_FILE) + intlen(pid) + 1) * sizeof(char));
+    sprintf(str, "%s%d", OUTPUT_FILE, pid);
+    int fd = open(str, O_WRONLY | O_CREAT, 0600);
+    if (fd == -1){
+        perror("open");
+        exit(EXIT_FAILURE);
     }
+    write(fd, nonce, strlen(nonce));
+    if (close(fd) == -1){
+        perror("close");
+        exit(EXIT_FAILURE);
+    }
+    free(nonce);
     exit(EXIT_SUCCESS);
 }
 
-int main (void){
-    for (int i = 0; i < 10; i++){
-        bruteforce(i, 10, DIFFICULTY);
+
+int main(void) {
+    pid_t ppid = getpid();
+    pid_t grp = -1;
+    char *result_file;
+    char buffer[READ_BUFFER];
+    int fd, status, size;
+    pid_t result_pid;
+
+    for (int i = 0; i < 10; i++) {
+        pid_t child = fork();
+        if (child < 0){
+            perror("fork");
+            exit(EXIT_FAILURE);
+        }
+        else if (child == 0){
+            bruteforce(i, 10, DIFFICULTY);
+        } else {
+            if (grp == -1){
+                grp = child;
+            }
+            if (setpgid(child, grp) == -1) {
+                perror("setpgid");
+            }
+        }
     }
+    while (1) {
+        result_pid = wait(&status);
+        if (WIFEXITED(status) && WEXITSTATUS(status) == EXIT_SUCCESS) {
+            break;
+        }
+    }
+    kill(-grp, SIGKILL);
+    result_file = malloc((strlen(OUTPUT_FILE) + intlen(result_pid) + 1) * sizeof(char));
+    sprintf(result_file, "%s%d", OUTPUT_FILE, result_pid);
+    fd = open(result_file, O_RDONLY);
+    if (fd == -1) {
+        perror("open");
+        exit(EXIT_FAILURE);
+    }
+    if ((size = read(fd, buffer, READ_BUFFER)) == -1) {
+        perror("read");
+        exit(EXIT_FAILURE);
+    }
+    if (close(fd) == -1) {
+        perror("close");
+        exit(EXIT_FAILURE);
+    }
+    buffer[size] = '\0';
+    printf("%s\n", buffer);
+    if (unlink(result_file) == -1) {
+        perror("unlink");
+        exit(EXIT_FAILURE);
+    }
+    exit(EXIT_SUCCESS);
 }
